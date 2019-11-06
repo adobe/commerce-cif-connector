8e152a24634b545a31e17b44691c899b664d0d7c
Levente Santha <sntha@adobe.com>
2019-11-06 15:55:58 +0200

    CIF-1065 - Preview PDP with real product data
     * added clientlib for calling the CIF product picker from JS code
     * added menu option for preview in Page Information menu of page editor
     * implemented preview action

---
+++ content/cif-connector/src/main/content/jcr_root/libs/commerce/gui/components/authoring/editor/pagepreview/clientlibs/pagepreview.js
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ *
+ *    Copyright 2019 Adobe. All rights reserved.
+ *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License. You may obtain a copy
+ *    of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ ******************************************************************************/
+
+/*
+ * Actions for the preview of product detail pages.
+ */
+
+(function(window, document, Granite, $) {
+    "use strict";
+
+    var relPdpPreview = ".cq-commerce-pdp-preview-activator";
+
+    var handlePdpPreview = function (e, data) {
+        if (!data) {
+            return;
+        }
+
+        var selections = data.selections;
+        var productSlug = null;
+        var paths = [];
+        if (Array.isArray(selections)) {
+            if (selections.length > 0 && selections[0]) {
+                productSlug = selections[0].value;
+            }
+        } else if (selections) {
+            productSlug = selections.value;
+        }
+
+        var url = Granite.author.ContentFrame.location;
+        var previewUrl = createPreviewUrl(url, productSlug);
+
+        if (previewUrl) {
+            window.open(previewUrl);
+        }
+    };
+
+    var createPreviewUrl = function(url, slug) {
+        if (!url || !slug) {
+            return null;
+        }
+
+        var indLastSlash = url.lastIndexOf("/");
+        if (indLastSlash < 0 || indLastSlash === url.length - 1) {
+            return null;
+        }
+
+        var urlPath = url.slice(0, indLastSlash + 1);
+        var urlName = url.substring(indLastSlash + 1);
+        var indHtml = urlName.lastIndexOf(".html");
+        var previewUrl = null;
+        if (indHtml > -1) {
+            var indSelector = urlName.slice(0, indHtml).lastIndexOf(".");
+            if (indSelector > -1) {
+                previewUrl = urlPath + urlName.slice(0, indSelector) + "." + slug + ".html";
+            } else {
+                previewUrl = urlPath + urlName.slice(0, indHtml) + "." + slug + ".html";
+            }
+        }
+
+        return previewUrl;
+    };
+
+    $(document).on("cifProductPickerSelection", relPdpPreview, handlePdpPreview);
+
+})(window, document, Granite, Granite.$);
---
+++ content/cif-connector/src/main/content/jcr_root/libs/commerce/gui/components/common/cifproductfield/clientlibs/cifproductpicker.js
@@ -0,0 +1,172 @@
+/*******************************************************************************
+ *
+ *    Copyright 2019 Adobe. All rights reserved.
+ *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License. You may obtain a copy
+ *    of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ ******************************************************************************/
+
+(function(window, document, Granite, $) {
+    "use strict";
+    /**
+     * The context component can specify the following properties for the picker:
+     * pickersrc, root, filter, selectioncount, selectionid.
+     * If pickersrc is specified then the root, filter, selectioncount and selectionid is ignored, they should be provided in
+     * the pickersrc.
+     */
+
+    /**
+     * Selector for CIF product picker context component
+     * @type {string}
+     */
+    var relActivator = ".cq-commerce-cifproductpicker-activator";
+
+    /**
+     * Event type for picker selection.
+     * @type {string}
+     */
+    var eventType = "cifProductPickerSelection";
+
+    /**
+     * Default picker URL.
+     * @type {string}
+     */
+    var defaultPickerSrc = "/mnt/overlay/commerce/gui/content/common/cifproductfield/picker.html";
+    /**
+     * Default root path shown by the picker when opened.
+     * @type {string}
+     */
+    var defaultRootPath = "/var/commerce/products";
+    var defaultSelectionCount = "single";
+    var defaultFilter = "folderOrProduct";
+    var defaultSelectionId = "id";
+
+    var show = function(control, state, handleSelections) {
+        state.api.attach(this);
+
+        state.api.pick(control[0], []).then(function(selections) {
+            handleSelections(selections);
+            close(control, state);
+
+        }, function() {
+            close(control, state, handleSelections);
+        });
+
+        if ("focus" in state.api) {
+            state.api.focus(last);
+        } else {
+            state.el.focus();
+        }
+
+        state.loading = false;
+        state.open = true;
+    };
+
+    var close = function(control, state, callback) {
+        state.api.detach();
+        state.open = false;
+        if (callback) {
+            callback();
+        }
+
+    };
+
+    var getState = function(control) {
+        var KEY_STATE =  relActivator + ".internal.state";
+
+        var state = control.data(KEY_STATE);
+
+        if (!state) {
+            state = {
+                el: null,
+                open: false,
+                loading: false
+            };
+            control.data(KEY_STATE, state);
+        }
+
+        return state;
+    };
+
+    var cifProductPicker = function(control, pickerSrc, handleSelections) {
+
+        var state = getState(control);
+
+        if (state.loading) {
+            return;
+        }
+
+        if (state.el) {
+            if (!state.open) {
+                show(control, state, handleSelections);
+            }
+        } else {
+            if (!pickerSrc) {
+                return;
+            }
+
+            state.loading = true;
+
+            $.ajax({
+                url: pickerSrc,
+                cache: false
+            }).then(function (html) {
+                return $(window).adaptTo("foundation-util-htmlparser").parse(html);
+            }).then(function (fragment) {
+                return $(fragment).children()[0];
+            }).then(function (pickerEl) {
+                state.el = pickerEl;
+                state.api = $(pickerEl).adaptTo("foundation-picker");
+
+                show(control, state, handleSelections);
+            }, function () {
+                state.loading = false;
+            });
+        }
+    };
+
+    $(document).on("click", relActivator, function (e) {
+        e.preventDefault();
+
+        var control = $(document).find(relActivator);
+        var test = control[0].dataset.foundationCollectionAction;
+        var pickerConfig = control.data();
+        var pickerSrc = pickerConfig.pickersrc;
+        if (!pickerSrc) {
+            pickerSrc = defaultPickerSrc;
+
+            var root = pickerConfig.root;
+            if (!root) {
+                root = defaultRootPath;
+            }
+            var filter = pickerConfig.filter;
+            if (!filter) {
+                filter = defaultFilter;
+            }
+            var selectionCount = pickerConfig.selectioncount;
+            if (!selectionCount) {
+                selectionCount = defaultSelectionCount;
+            }
+
+            var selectionId = pickerConfig.selectionid;
+            if (!selectionId) {
+                selectionId = defaultSelectionId;
+            }
+
+            pickerSrc = pickerSrc + "?root=" + root + "&filter=" + filter + "&selectionCount=" + selectionCount +
+                "&selectionId=" + selectionId;
+        }
+        var handleSelections = function(selections) {
+            control.trigger(eventType, {"selections": selections});
+        };
+
+        cifProductPicker(control, pickerSrc, handleSelections);
+    });
+
+})(window, document, Granite, Granite.$);
