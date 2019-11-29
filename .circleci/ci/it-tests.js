4e201f9a0f099f1ba4a95e4b7449d50265d8235f
Mark J. Becker <herzog31@users.noreply.github.com>
2019-11-29 16:59:10 +0100

    Add Jacoco coverage collection

--- .circleci/ci/it-tests.js
+++ .circleci/ci/it-tests.js
@@ -42,7 +42,7 @@ ci.dir(qpPath, () => {
         --install-file ${path.resolve(buildPath, 'content/cif-connector/target', `cif-connector-content-${config.modules['cif-connector-content'].version}.zip`)} \
         --install-file ${path.resolve(buildPath, 'content/cif-virtual-catalog/target', `cif-virtual-catalog-content-${config.modules['cif-virtual-catalog-content'].version}.zip`)} \
         --install-file ${path.resolve(buildPath, 'it/content/target', `it-test-content-${config.modules['it-test-content'].version}.zip`)} \
-        --vm-options '-javaagent:${process.env.JACOCO_AGENT}=destfile=jacoco-it.exec`);
+        --vm-options '-javaagent:${process.env.JACOCO_AGENT}=destfile=jacoco-it.exec'`);
 });
 
 // Run integration tests
