22e23bb228d38ffdaad29fcda95f45d638882e31
Laurentiu Magureanu <magurean@adobe.com>
2020-07-20 17:51:21 +0300

    CIF-1326: Updated configuration clientlib  - Gets the inheritance config from the servlet  - Adds lock controls to fields  - sets values and locks accordingly  - handles @Delete fields

--- content/cif-connector/src/main/content/jcr_root/libs/commerce/gui/components/configuration/page/clientlibs/js/configurationpage.js
+++ content/cif-connector/src/main/content/jcr_root/libs/commerce/gui/components/configuration/page/clientlibs/js/configurationpage.js
@@ -37,6 +37,31 @@
     let commerceProviderCoralSelectComponent;
 
     /**
+     * Coral Select component used for selecting the graphQL client.
+     */
+    let graphQlClient;
+
+    /**
+     * Textfield component used for providing a Store View identifier.
+     */
+    let language;
+
+    /**
+     * Textfield component used for providing a GraphQL Proxy Path.
+     */
+    let graphQlProxyPath;
+
+    /**
+     * Textfield component used for providing a Catalog Root Category Idr.
+     */
+    let magentoRootCategoryId;
+
+    /**
+     * Textfield component used for providing a Language.
+     */
+    let storeView;
+
+    /**
      * Coral Textfield component user for providing a name for the new node.
      */
     let nameCoralTextfieldComponent;
@@ -52,21 +77,71 @@
     let redirectLocation;
 
     /**
+     * The properties map JCR key -> HTML element.
+     */
+    let propertiesMap;
+
+    /**
+     * The properties map of inherited resource.
+     */
+    let inheritedValues;
+
+    /**
+     * Inheritance handling state. 0 - not started, 1 - in progress, 2 - Done.
+     * @type {number}
+     */
+    let inheritanceHandled = 0;
+
+    /**
      * Collects references to DOM elements and Coral components. Meant to be called after foundation-contentloaded
      * event is triggered.
      */
     function init() {
         formElement = $('#cq-commerce-products-bindproducttree-form');
         catalogIdentifierCoralSelectComponent = $('#cq-commerce-products-bindproducttree-catalog-select').get(0);
-        catalogIdentifierDataCoralSelectComponent = $('#cq-commerce-products-bindproducttree-catalog-select-data').get(
-            0
-        );
+        catalogIdentifierDataCoralSelectComponent = $('#cq-commerce-products-bindproducttree-catalog-select-data').get(0);
         commerceProviderCoralSelectComponent = $('#cq-commerce-products-bindproducttree-provider-select').get(0);
+        graphQlClient = $("#cq-commerce-graphql-client").get(0);
+        storeView = $('input[name="./magentoStore"]').get(0);
+        graphQlProxyPath = $('input[name="./magentoGraphqlEndpoint"]').get(0);
+        magentoRootCategoryId = $('input[name="./magentoRootCategoryId"]').get(0);
+        language = $('coral-select[name="./jcr:language"]').get(0);
         nameCoralTextfieldComponent = $('#cq-commerce-products-bindproducttree-name').get(0);
         redirectLocation = $('#cq-commerce-products-bindproducttree-form')
             .find('[data-foundation-wizard-control-action=cancel]')
             .attr('href');
         catalogIdentifierHidden = $('#cq-commerce-products-bindproducttree-catalog-hidden').get(0);
+
+        propertiesMap = {
+            "cq:catalogDataResourceProviderFactory": {
+                element: commerceProviderCoralSelectComponent,
+                locked: false
+            },
+            "cq:catalogIdentifier": {
+                element: catalogIdentifierCoralSelectComponent,
+                locked: false
+            },
+            "cq:graphqlClient": {
+                element: graphQlClient,
+                locked: false
+            },
+            "magentoStore": {
+                element: storeView,
+                locked: false
+            },
+            "magentoGraphqlEndpoint": {
+                element: graphQlProxyPath,
+                locked: false
+            },
+            "magentoRootCategoryId": {
+                element: magentoRootCategoryId,
+                locked: false
+            },
+            "jcr:language": {
+                element: language,
+                locked: false
+            }
+        }
     }
 
     /**
@@ -96,15 +171,17 @@
      * Given a list of available values, builds a list of Coral Select items.
      *
      * @param {string[]} values The list of values which will be converted to a list of Coral Select items.
+     * @param {string} selected value of Coral Select.
      * @returns {Object[]} The list of Coral Select items.
      */
-    function buildSelectItems(values) {
+    function buildSelectItems(values, selected) {
         return values.map(catalog => {
             return {
                 value: catalog,
                 content: {
                     textContent: catalog
-                }
+                },
+                selected: (catalog === selected)
             };
         });
     }
@@ -132,27 +209,210 @@
     }
 
     /**
+     * Returns the lock status of a field by it's name
+     * @param name
+     * @returns {boolean|locked}
+     */
+    function getLockStatus(name) {
+        return propertiesMap[name].locked;
+    }
+
+    /**
      * Populates the commerce identifier select component after the commerce provider is selected.
      */
     function commerceProviderSelectedHandler() {
+        const catalogIdentifierLocked = getLockStatus("cq:catalogIdentifier");
         const commerceProvider = getSelectedCommerceProvider();
-        if (commerceProvider !== '') {
-            const catalogs = getCatalogIdentifiers(commerceProvider);
 
-            catalogIdentifierCoralSelectComponent.items.clear();
-            const items = buildSelectItems(catalogs);
-            items.forEach(item => {
-                catalogIdentifierCoralSelectComponent.items.add(item);
-            });
-            catalogIdentifierCoralSelectComponent.disabled = false;
-        } else {
-            catalogIdentifierCoralSelectComponent.disabled = true;
+        if (!catalogIdentifierLocked && commerceProvider !== '') {
+            updateCatalogIdentifier();
+        }
+
+        if (inheritanceHandled === 0) {
+            catalogIdentifierCoralSelectComponent.disabled = (commerceProvider === '');
         }
     }
 
+    /**
+     * Handles change events for Catalog Identifier field
+     */
     function catalogIdentifierSelectedHandler() {
         const catalogIdentifier = catalogIdentifierCoralSelectComponent.selectedItem.value;
         catalogIdentifierHidden.value = catalogIdentifier;
+    }
+
+    /**
+     * Updates the options and selected value of the Catalog Identifier Select
+     * If a parameter is passed and matches any of the fields being added to the select options, that option will be selected
+     * @param selected
+     */
+    function updateCatalogIdentifier(selected) {
+        const commerceProvider = getSelectedCommerceProvider();
+        const catalogs = getCatalogIdentifiers(commerceProvider);
+        catalogIdentifierCoralSelectComponent.items.clear();
+        const items = buildSelectItems(catalogs, selected);
+        items.forEach(item => {
+            catalogIdentifierCoralSelectComponent.items.add(item);
+        });
+    }
+
+    /**
+     * Function for displaying the inheritance break/revert dialog for confirmation
+     * @param linkElement
+     */
+    function showInheritanceDialog(linkElement) {
+        const dialog = document.getElementById('inheritance-dialog');
+        dialog.header.innerHTML = linkElement.title;
+        dialog.content.innerHTML = '<p>Do you really want to '+linkElement.title.split(" ")[0].toLowerCase()+' the inheritance?</p>';
+
+        const confirmButton = dialog.footer.querySelector("#inheritance-change");
+        confirmButton.dataset.targetProperty = linkElement.dataset.togglePropertyInheritance;
+        confirmButton.dataset.locked = linkElement.dataset.inheritanceLocked;
+
+        dialog.show();
+    }
+
+    /**
+     * Function used to generate a link icon for toggling the inheritance
+     * inheritance status is passed via `data` param
+     * @param data
+     * @returns {*|jQuery}
+     */
+    function generateInheritanceControl(data) {
+        const iconSuffix = data.locked ? '' : 'Off';
+        return $(
+            '<a class="cif-toggle-inheritance" ' +
+                    'data-toggle-property-inheritance="' + data.property + '" ' +
+                    'data-inheritance-locked="' + data.locked + '" ' +
+                    'title="' + (data.locked ? 'Cancel inheritance' : 'Revert inheritance') + '" href="#">' +
+                '<coral-icon class="coral3-Icon coral3-Icon--link' + iconSuffix + ' coral3-Icon--sizeS" ' +
+                        'icon="link' + iconSuffix + '" size="S" role="img" aria-label="link ' + iconSuffix.toLowerCase() + '">' +
+                '</coral-icon>' +
+            '</a>').get(0);
+    }
+
+    /**
+     * Sets the lock status and updates value for a given fields's inheritance toggle link
+     * @param linkElement
+     * @param status
+     */
+    function setLockStatus(linkElement, status) {
+        const propertyName = linkElement.dataset.togglePropertyInheritance;
+        document.querySelector('input[name="./' + propertyName + '@Delete"]').disabled = !status;
+        propertiesMap[propertyName].element.disabled = status;
+        propertiesMap[propertyName].locked = status;
+
+        if (propertyName === 'cq:catalogIdentifier') {
+            catalogIdentifierHidden.disabled = status;
+        }
+
+        linkElement.dataset.inheritanceLocked = status.toString();
+        linkElement.title = (!status ? 'Cancel inheritance' : 'Revert inheritance');
+
+        const iconElement = linkElement.querySelector('coral-icon');
+
+        iconElement.classList.remove((status ? "coral3-Icon--linkOff" : "coral3-Icon--link"));
+        iconElement.classList.add((status ? "coral3-Icon--link" : "coral3-Icon--linkOff"));
+        iconElement.setAttribute("aria-label", (status ? "link" : "link off"));
+        iconElement.setAttribute("icon", (status ? "link" : "linkOff"));
+
+        if (inheritanceHandled === 2 && status) {
+            if (propertyName === 'cq:catalogIdentifier') {
+                updateCatalogIdentifier(inheritedValues[propertyName].value);
+            } else {
+                propertiesMap[propertyName].element.value = inheritedValues[propertyName].value;
+            }
+
+            propertiesMap[propertyName].element.trigger('change');
+        }
+    }
+
+    /**
+     * Sets the initial field value based on the inheritance status
+     * @param key
+     */
+    function setInitialValue(key) {
+        const fieldData = propertiesMap[key];
+        if (key === 'cq:catalogIdentifier') {
+            const value = fieldData.locked ? inheritedValues[key].value : catalogIdentifierHidden.value;
+            updateCatalogIdentifier(value);
+            catalogIdentifierCoralSelectComponent.trigger('change');
+        } else {
+            fieldData.element.value = fieldData.locked ? inheritedValues[key].value : fieldData.element.value;
+        }
+    }
+
+    /**
+     * Handles the response from the inheritance status servlet
+     * Generates inheritance toggle links, sets initial field values, sets the lock status and adds event handlers
+     * @param data
+     */
+    function handleInheritanceReceieved(data) {
+        if (inheritanceHandled === 0 && data.inherited) {
+            inheritanceHandled = 1;
+            inheritedValues = data.inheritedProperties;
+            Object.keys(propertiesMap).forEach(function(key) {
+                const elem = propertiesMap[key].element;
+                elem.classList.add("cif-lockable-field");
+                propertiesMap[key].locked = data.overriddenProperties.indexOf(key) === -1;
+                setInitialValue(key);
+                const lockControl = generateInheritanceControl({
+                    locked: propertiesMap[key].locked,
+                    property: key
+                });
+                lockControl.addEventListener('click', function (event) {
+                    showInheritanceDialog(event.currentTarget);
+                });
+
+                elem.parentElement.appendChild(lockControl);
+                setLockStatus(lockControl, propertiesMap[key].locked);
+            });
+
+            const dialog = new Coral.Dialog().set({
+                id: "inheritance-dialog",
+                header: {
+                    innerHTML: ""
+                },
+                content: {
+                    innerHTML: ""
+                },
+                footer: {
+                    innerHTML: "<button is=\"coral-button\" coral-close=\"\" class=\"coral3-Button coral3-Button--secondary\" size=\"M\" variant=\"secondary\"><coral-button-label>No</coral-button-label></button>"
+                },
+                variant: "warning"
+            });
+
+            const actionButton = new Coral.Button().set({
+                id: "inheritance-change",
+                variant: "primary",
+                label: {
+                    innerHTML: '<coral-button-label>Yes</coral-button-label>'
+                }
+            })
+
+            actionButton.on('click', function () {
+                const linkElement = document.querySelector('a[data-toggle-property-inheritance="'+this.dataset.targetProperty+'"]');
+                setLockStatus(linkElement, (this.dataset.locked !== 'true'));
+                dialog.hide();
+            })
+
+            dialog.footer.appendChild(actionButton);
+            document.body.appendChild(dialog);
+
+            inheritanceHandled = 2;
+        }
+    }
+
+    /**
+     * Builds the URL and performs an AJAX call to the inheritance status servlet
+     */
+    function fetchInheritance() {
+        const urlParams = new URLSearchParams(window.location.search);
+        var resourcePath = urlParams.has("item") ?
+            decodeURIComponent(urlParams.get("item")) : document.querySelector('input[name="parentPath"]').value;
+        resourcePath += '.cifconfig.json';
+
+        $.get(resourcePath).done(handleInheritanceReceieved)
     }
 
     $(document).on('foundation-contentloaded', () => {
@@ -165,25 +425,27 @@
                     let commerceProvider = commerceProviderCoralSelectComponent.selectedItem
                         ? commerceProviderCoralSelectComponent.selectedItem.value
                         : '';
+
                     if (commerceProvider && commerceProvider.length > 0) {
                         const catalogIdentifiers = getCatalogIdentifiers(commerceProvider);
                         const catalogIdentifier = catalogIdentifiers.find(
                             item => item === catalogIdentifierHidden.value
                         );
-                        catalogIdentifierCoralSelectComponent.items.clear();
-                        catalogIdentifierCoralSelectComponent.items.add({
-                            value: catalogIdentifier,
-                            content: {
-                                textContent: catalogIdentifier
-                            },
-                            selected: true
-                        });
+
+                        updateCatalogIdentifier(catalogIdentifier);
                     }
                 }
                 commerceProviderCoralSelectComponent.on('change', commerceProviderSelectedHandler);
                 catalogIdentifierCoralSelectComponent.on('change', catalogIdentifierSelectedHandler);
+
+                Object.keys(propertiesMap).forEach(function (key) {
+                    document.querySelector('input[name="./' + key + '@Delete"]').disabled = true;
+                })
+
+                fetchInheritance();
             });
         }
+
         formElement.on('submit', formSubmitHandler);
     });
 })(window, document, Granite, Granite.$);
